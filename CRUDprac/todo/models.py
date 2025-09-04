899172478700f99267408f38df2c1133cc50d668
TaeGyoung Lee <90079870+taeg2@users.noreply.github.com>
2025-09-04 11:00:24 +0000

    todo 초기 세팅하기

---
+++ CRUDprac/todo/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+
+# Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    complete = models.BooleanField(default=False)
